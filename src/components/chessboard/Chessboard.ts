c305fa2453f372870e6eddb10016e5a5830b115c
mikilukasik <szarhazi@gmail.com>
2023-03-18 05:19:01 +0000

    responsive chessboard

--- src/components/chessboard/Chessboard.ts
+++ src/components/chessboard/Chessboard.ts
@@ -6,23 +6,28 @@ const chessboardState = createState({
   fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
 });
 
-export const Chessboard = component(() => {
-  const pureFen = chessboardState.fen.split(" ")[0].split("/").join("");
-  let boardContent = "";
+export const Chessboard = component(
+  () => {
+    const pureFen = chessboardState.fen.split(" ")[0].split("/").join("");
+    let boardContent = "";
 
-  let col = 0;
-  for (const char of pureFen) {
-    if (isNaN(parseInt(char))) {
-      boardContent += BoardSquare({ index: col, char });
-      col += 1;
-      continue;
+    let col = 0;
+    for (const char of pureFen) {
+      if (isNaN(parseInt(char))) {
+        boardContent += BoardSquare({ index: col, char });
+        col += 1;
+        continue;
+      }
+
+      for (let j = 0; j < parseInt(char); j++) {
+        boardContent += BoardSquare({ index: col });
+        col += 1;
+      }
     }
 
-    for (let j = 0; j < parseInt(char); j++) {
-      boardContent += BoardSquare({ index: col });
-      col += 1;
-    }
+    return html`<div class="board">${boardContent}</div>`;
+  },
+  {
+    class: "board-container",
   }
-
-  return html`<div class="board">${boardContent}</div>`;
-});
+);
