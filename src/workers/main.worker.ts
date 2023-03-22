595454909f3e46d3867c85582a8813612446e095
mikilukasik <szarhazi@gmail.com>
2023-03-22 16:15:06 +0000

    loads a tfjs model in worker

--- src/workers/main.worker.ts
+++ src/workers/main.worker.ts
@@ -1,3 +1,9 @@
+import * as tf from "@tensorflow/tfjs";
+
 console.log("im the worker");
 
 postMessage("this is the message to main");
+
+(async () => {
+  tf.loadLayersModel("tfjs_model/model.json");
+})();
