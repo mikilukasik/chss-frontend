19b87907361d94ac6c8b27e70413e748c37c7894
mikilukasik <szarhazi@gmail.com>
2023-05-06 17:05:04 +0100

    better model ratios in defaultengine

--- src/components/chessboard/helpers/chessboardState.ts
+++ src/components/chessboard/helpers/chessboardState.ts
@@ -19,8 +19,8 @@ declare global {
 const defaultEngineConfig = {
   depth: 5,
   moveSorters: [{ cutoff: 0.01 }],
-  moveScoreRario: 5,
-  winnerScoreRario: 1,
+  moveScoreRario: 3,
+  winnerScoreRario: 0.5,
 };
 
 const lastRememberedState = localStorage.getItem("chessBoardState");
