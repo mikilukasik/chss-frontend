3c1e8379b0cf26baa77e851f4018bf63222b5d74
mikilukasik <mikilukasik@users.noreply.github.com>
2023-05-08 01:20:26 +0100

    1.07/0.52

--- src/components/chessboard/helpers/chessboardState.ts
+++ src/components/chessboard/helpers/chessboardState.ts
@@ -19,8 +19,8 @@ declare global {
 const defaultEngineConfig = {
   depth: 5,
   moveSorters: [{ cutoff: 0.01 }],
-  moveScoreRario: 0.73,
-  winnerScoreRario: 0.15,
+  moveScoreRario: 1.07,
+  winnerScoreRario: 0.52,
 };
 
 const lastRememberedState = localStorage.getItem("chessBoardState");
